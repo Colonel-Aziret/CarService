978f0323cc9c4c5fdf5a642bb6188e93ed5e7182
Aziret <Aziret5265@gmail.com>
2023-03-21 21:01:01 +0600

    add application properties

--- src/main/java/com/example/carservice/CarServiceApplication.java
+++ src/main/java/com/example/carservice/CarServiceApplication.java
@@ -1,6 +1,6 @@
 package com.example.carservice;
 
-import org.springframework.boot.SpringApplication;
+import  org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
